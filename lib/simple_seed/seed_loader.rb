d3986508aec2c4b25baa276d0937fb53f247b50c
Gen Takahashi <gendosu@gmail.com>
2020-04-26 15:29:59 +0900

    Once an environment is configured, load data only in that environment

--- lib/simple_seed/seed_loader.rb
+++ lib/simple_seed/seed_loader.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module SimpleSeed
   class SeedLoader
     def self.load_seed
@@ -5,14 +7,26 @@ module SimpleSeed
     end
 
     def self.import_fixture(name, environments = nil)
+      env_list = if environments.is_a?(Array)
+                   environments.map(&:to_s)
+                 elsif !environments
+                   []
+                 else
+                   [environments.to_s]
+                 end
+
+      return if environments && !env_list.include?(Rails.env.downcase)
+
       puts "Import #{name}..."
-      fixtures_path = if environments
-                        Rails.root.join('db', 'fixtures', environments.to_s)
-                      else
-                        Rails.root.join('db', 'fixtures')
-                      end
-      ActiveRecord::FixtureSet.create_fixtures \
-      fixtures_path, name
+
+      if !environments
+        ActiveRecord::FixtureSet.create_fixtures \
+          Rails.root.join('db', 'fixtures'), name
+
+      elsif env_list.include?(Rails.env.downcase)
+        ActiveRecord::FixtureSet.create_fixtures \
+          Rails.root.join('db', 'fixtures', Rails.env.downcase), name
+      end
     end
   end
 end
